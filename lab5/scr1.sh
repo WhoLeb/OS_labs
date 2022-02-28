e471d5ce24c8ef94bb22c32f441f388861e9510f
WhoLeb <gleb_lyah@mail.ru>
2022-02-28 16:14:45 +0300

    changed scripts a bit

--- lab5/scr1.sh
+++ lab5/scr1.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-rm output_file
-gnome-terminal -- ./scr2.sh
 g++ -c fir.cpp -o lab.o
 g++ -o lab.obj lab.o -pthread
 ./lab.obj
---
+++ lab5/script.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+rm output_file
+gnome-terminal -- ./scr1.sh
+gnome-terminal -- ./scr2.sh
